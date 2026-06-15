d0aa45650ac17b76481bb0a8a12eb7771aaa83ad
Yadollah Zekawat <zekawat.py@gmail.com>
2026-06-15 14:32:57 +0330

    Adding Hierarchical Data Changes to the Admin Panel Post Model

--- blog/admin.py
+++ blog/admin.py
@@ -4,13 +4,20 @@ from .models import Category, Tag, Post
 # ------------> Models Registered<------------
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ('name',)
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ('name',)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'status', 'author', 'published_at', 'created_at')
+    list_filter = ('status', 'created_at', 'category')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ('created_at', 'updated_at')
+    date_hierarchy = 'published_at'
 

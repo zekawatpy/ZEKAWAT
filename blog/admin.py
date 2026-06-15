69573a4b575946a8c714e4df4e9773fe4241faa7
Yadollah Zekawat <zekawat.py@gmail.com>
2026-06-15 14:43:29 +0330

    add list_display to category and tag models

--- blog/admin.py
+++ blog/admin.py
@@ -4,11 +4,13 @@ from .models import Category, Tag, Post
 # ------------> Models Registered<------------
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
     prepopulated_fields = {"slug": ("name",)}
     search_fields = ('name',)
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
     prepopulated_fields = {"slug": ("name",)}
     search_fields = ('name',)
 
